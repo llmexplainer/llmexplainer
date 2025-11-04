9a4fe4beb24e8da5e865bf7631f33f533c5ca332
Sarah Al-Yahya <sarahalyahya18@gmail.com>
2025-11-04 10:15:17 +0300

    personality key

--- public/dataHelpers.js
+++ public/dataHelpers.js
@@ -5,14 +5,23 @@ async function loadDataContent(){
     const response = await fetch ("json/dataContent.json");
     const data = await response.json(); 
     DATA_TYPES = data.DATA_TYPES;
+    FEEDBACK_QUESTIONS = data.FEEDBACK_QUESTIONS;
 
-    console.log("Data loaded:", DATA_TYPES);
+    console.log("Data loaded:", DATA_TYPES, FEEDBACK_QUESTIONS);
 
+}
+
+function getPersonalityKey(randomness,friendliness,wordiness){
+    const r = randomness > 50? 'h' : 'l'; //>=??? 
+    const f = friendliness > 50?  'h':'l';
+    const w = wordiness > 50? 'h':'l';
+
+    return(`${r}-${f}-${w}`)
 }
 
 
 //helper functions needed:
-// [] converting slider values and returning the personality
+// [x] converting slider values and returning the personality code (h-l-h,h-h-h...etc.)
 // [] getting the text in the finetuning step that's based on personality
 // [] getting chatbot answers 
 // [] loading feedback qs
