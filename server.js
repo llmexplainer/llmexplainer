9e24e538cf809f7b3d3b31e49daec5c1e448e6db
Alia ElKattan <aae322@nyu.edu>
2025-11-08 11:19:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 
 app.listen(PORT, () => {
   console.log(`server running at port:${PORT}`);
 });
-
-
